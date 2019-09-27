44cf2f268835cdc518d81133b58768c067d572b9
Chai-Jin <30280549+Chai-Jin@users.noreply.github.com>
2019-09-27 14:35:29 +0900

    Update hisat2.sh

--- hisat2.sh
+++ hisat2.sh
@@ -5,7 +5,7 @@ set -ueo pipefail
 CPUS=8
 
 # The hisat index name.
-IDX=/home/jung/refs/barley_ASM32608v1_ht2_ref
+IDX=/data/Public/refs/ht2_ref/mm10/genome
 
 # Keep track of what the aligners printed out.
 RUNLOG=runlog.txt
@@ -15,10 +15,10 @@ echo "Run started by 'whoami' on 'date'" > $RUNLOG
 # HISAT2 run on PE samples.
 for SAMPLE in $(cat sample.txt)
 do
- R1=/home/jung/sccbf/barley/fastq/${SAMPLE}_1.fastq
- R2=/home/jung/sccbf/barley/fastq/${SAMPLE}_2.fastq
- BAM=/home/jung/sccbf/barley/bam/${SAMPLE}.bam
- SUMMARY=/home/jung/sccbf/barley/fastq/${SAMPLE}_summary.txt
+ R1=/data/project/chaijin/PSMD5/fastq/${SAMPLE}_1.fastq
+ R2=/data/project/chaijin/PSMD5/fastq/${SAMPLE}_2.fastq
+ BAM=/data/project/chaijin/PSMD5/fastq/bam/${SAMPLE}.bam
+ SUMMARY=/data/project/chaijin/PSMD5/fastq/${SAMPLE}_summary.txt
 
  echo "Running HISAT2 on paired end: $SAMPLE"
  hisat2 -p $CPUS --dta -x $IDX -1 $R1 -2 $R2 | samtools sort > $BAM 2> $RUNLOG
